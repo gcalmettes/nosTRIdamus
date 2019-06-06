ac05c9bb61ff9ff66598269f5abc11bd97912d24
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-06-06 08:40:35 -0700

    notebooks for the different tasks

---
+++ notebooks/missing_races.jl
@@ -0,0 +1,12 @@
+{"id": "buffalosprings70.3", "name": "IRONMAN 70.3 Buffalo Springs Lake", "website": "http://www.ironman.com/triathlon/events/americas/ironman-70.3/inactive/buffalo-springs-lake.aspx"}
+{"id": "traversecity70.3", "name": "IRONMAN 70.3 Traverse City", "website": "http://www.ironman.com/triathlon/events/americas/ironman-70.3/traverse-city.aspx"}
+{"id": "jeju70.3", "name": "IRONMAN 70.3 Goseong Korea", "website": "http://www.ironman.com/triathlon/events/asiapac/ironman-70.3/goseong-korea.aspx"}
+{"id": "stcroix70.3", "name": "IRONMAN 70.3 St. Croix", "website": "http://www.ironman.com/triathlon/events/americas/ironman-70.3/inactive/st.-croix.aspx"}
+{"id": "hefei70.3", "name": "IRONMAN 70.3 Hefei", "website": "http://www.ironman.com/triathlon/events/asiapac/ironman-70.3/hefei.aspx"}
+{"id": "korea70.3", "name": "IRONMAN 70.3 Gurye Korea", "website": "http://www.ironman.com/triathlon/events/asiapac/ironman-70.3/gurye-korea.aspx"}
+{"id": "chungju70.3", "name": "IRONMAN 70.3 Chungju", "website": "http://www.ironman.com/triathlon/events/asiapac/ironman-70.3/inactive/chungju-korea.aspx"}
+{"id": "Vineman", "name": "IRONMAN Vineman", "website": "http://www.ironman.com/triathlon/events/americas/ironman/vineman.aspx"}
+{"id": "busan70.3", "name": "IRONMAN 70.3 Busan", "website": "http://www.ironman.com/triathlon/events/asiapac/ironman-70.3/inactive/busan-korea.aspx"}
+{"id": "putrajaya70.3", "name": "IRONMAN 70.3 Putrajaya", "website": "http://www.ironman.com/triathlon/events/asiapac/ironman-70.3/inactive/putrajaya.aspx"}
+{"id": "ireland70.3", "name": "IRONMAN 70.3 Galway", "website": "http://www.ironman.com/triathlon/events/emea/ironman-70.3/inactive/galway.aspx"}
+{"id": "monaco70.3", "name": "IRONMAN 70.3 Monaco", "website": "http://www.ironman.com/triathlon/events/emea/ironman-70.3/inactive/monaco.aspx"}
