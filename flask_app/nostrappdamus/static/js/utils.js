24abbacf1ec4f05246d6bd8e7fe35038060ddae3
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-06-13 11:34:38 -0700

    merge requests in one commun function

--- flask_app/nostrappdamus/static/js/utils.js
+++ flask_app/nostrappdamus/static/js/utils.js
@@ -1,13 +1,24 @@
-const postData = (url = '', data = {}) => {
-  return fetch(url, {
+const sendRequest = config => {
+  const { url = '', args = {}, method = 'POST' } = config
+  let request
+  if (method == 'POST') {
+    request = fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(args),
     })
-    .then(response => response.json())
+  } else if (method == 'GET') {
+    const queryString = `${url}?` + Object.entries(args)
+      .map(([key,value]) => `${key}=${value}`)
+      .join('&')
+
+    request = fetch(queryString, {method: 'GET'})
+  }
+  return request.then(response => response.json())
 }
+
 
 const createTableRow = data => {
   const row = document.createElement('tr')
@@ -22,39 +33,3 @@ const createTableRow = data => {
 
   return row
 }
-
-// const submit = (random=false) => {
-
-//   // radio buttons selection
-//   const selectedValue = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
-
-//   // input from text box
-//   const text = document.getElementById('method').value
-
-//   // result table
-//   const resultTable = document.getElementById('result-table')
-
-//   const url = random 
-//     ? 'http://127.0.0.1:5000/data/random'
-//     : 'http://127.0.0.1:5000/data'
-
-//   return postData(url, {filterBy: text, selection: selectedValue})
-//     .then(data => {
-//       resultTable.querySelectorAll('tr')
-//         .forEach((d, i) => i > 0 && d.remove())
-
-//       const results = data.data ? data.data.slice(1, 10) : []
-//       results
-//         .forEach(d => {
-//           const row = createTableRow({
-//             'birth_month': d.birth_month,
-//             'attendant': d.attendant,
-//             'weight': d.birth_weight,
-//             'year': d.birth_year
-//           })
-//           resultTable.appendChild(row)
-//         })
-//       return data
-//     })
-//     .catch(error => console.error(error))
-//   }
