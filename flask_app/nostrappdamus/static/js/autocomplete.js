bdafe21a7e51eb034824f0f855f12c56233abf98
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-06-27 18:56:56 -0700

    boldify the correct letters when autocompleting

--- flask_app/nostrappdamus/static/js/autocomplete.js
+++ flask_app/nostrappdamus/static/js/autocomplete.js
@@ -1,18 +1,18 @@
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
-  var currentFocus;
+  let currentFocus;
   /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
+  inp.addEventListener('input', function(e) {
+      let a, b, i, val = this.value;
       /*close any already open lists of autocompleted values*/
       closeAllLists();
       if (!val) { return false;}
       currentFocus = -1;
       /*create a DIV element that will contain the items (values):*/
       a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
+      a.setAttribute('id', `${this.id}autocomplete-list`);
+      a.setAttribute('class', 'autocomplete-items');
       /*append the DIV element as a child of the autocomplete container:*/
       this.parentNode.appendChild(a);
       /*for each item in the array...*/
@@ -20,18 +20,26 @@ function autocomplete(inp, arr) {
         /*check if the item starts with the same letters as the text field value:*/
         // if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
         if (arr[i].toUpperCase().includes(val.toUpperCase())) {          
-
           /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
+          b = document.createElement('DIV');
           /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
+          const pos = arr[i].toUpperCase().indexOf(val.toUpperCase())
+          if (pos == 0) {
+            b.innerHTML = `<strong>${arr[i].substr(0, val.length)}</strong>`;
+            b.innerHTML += arr[i].substr(val.length);
+          } else {
+            b.innerHTML = arr[i].slice(0, pos);
+            b.innerHTML += `<strong>${arr[i].substr(pos, val.length)}</strong>`;
+            b.innerHTML += arr[i].substr(pos + val.length);
+          }
+          // b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          // b.innerHTML += arr[i].substr(val.length);
           /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.innerHTML += `<input type="hidden" value="${arr[i]}">`;
           /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
+              b.addEventListener('click', function(e) {
               /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
+              inp.value = this.getElementsByTagName('input')[0].value;
               /*close the list of autocompleted values,
               (or any other open lists of autocompleted values:*/
               closeAllLists();
@@ -41,8 +49,8 @@ function autocomplete(inp, arr) {
       }
   });
   /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
+  inp.addEventListener('keydown', function(e) {
+      let x = document.getElementById(`${this.id}autocomplete-list`);
       if (x) x = x.getElementsByTagName("div");
       if (e.keyCode == 40) {
         /*If the arrow DOWN key is pressed,
@@ -73,26 +81,26 @@ function autocomplete(inp, arr) {
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
     /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
+    x[currentFocus].classList.add('autocomplete-active');
   }
   function removeActive(x) {
     /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
+    for (let i = 0; i < x.length; i++) {
+      x[i].classList.remove('autocomplete-active');
     }
   }
   function closeAllLists(elmnt) {
     /*close all autocomplete lists in the document,
     except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
+    let x = document.getElementsByClassName('autocomplete-items');
+    for (let i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
       x[i].parentNode.removeChild(x[i]);
       }
     }
   }
   /*execute a function when someone clicks in the document:*/
-  document.addEventListener("click", function (e) {
+  document.addEventListener('click', function (e) {
       closeAllLists(e.target);
   });
 }
