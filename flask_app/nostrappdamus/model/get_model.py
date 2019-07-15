db3f5eea38b6acd62a7d889932e070315cd48777
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-07-15 14:09:01 -0700

    PEP8 compliant

--- flask_app/nostrappdamus/model/get_model.py
+++ flask_app/nostrappdamus/model/get_model.py
@@ -1,6 +1,5 @@
 import pickle
 import scipy.sparse
-import numpy as np
 import pandas as pd
 import json
 
@@ -20,7 +19,7 @@ models_list = {
         'name': 'KNN content-based',
         'model': None,
         'df': {
-            'file': './nostrappdamus/model/data/knn_content_df.csv', 
+            'file': './nostrappdamus/model/data/knn_content_df.csv',
             'index_col': 'race'
         },
         'matrix': None,
@@ -35,9 +34,10 @@ look_up_items = {
     'index_col': 'race'
 }
 
-# the first time it will be called, the variable will be assigned 
+# the first time it will be called, the variable will be assigned
 items = None
 items_map = None
+
 
 def get_model(model_name):
     config = models_list.get(model_name)
@@ -47,12 +47,12 @@ def get_model(model_name):
         model_file = config.get('model')
         # df/matrix feeding the model
         model_df = config.get('df', False)
-        matrix_file = config.get('matrix', False) 
+        matrix_file = config.get('matrix', False)
         load = config.get('load_matrix', False)
         model_hash_map = config.get('hash_map', False)
 
     # load model
-    if type(model_file) != type(None):
+    if model_file is not None:
         with open(model_file, 'rb') as f:
             trained_model = pickle.load(f)
     else:
@@ -72,37 +72,45 @@ def get_model(model_name):
 
     # make sure the items have been loaded into the variable space
     global items
-    if type(items) == type(None):
+    if items is not None:
         get_items()
 
-    model = model_class(trained_model, matrix=matrix, items_info=items, pos_to_item_mapping=hash_map, df=df, name=model_name)
+    model = model_class(trained_model, matrix=matrix, items_info=items,
+                        pos_to_item_mapping=hash_map, df=df, name=model_name)
 
     return model
 
 
 def get_items():
     global items, items_map
-    if type(items) != type(None):
+    if items is not None:
         return items
     else:
         print('Loading items data for the first time!')
         # load races info
-        items_full = pd.read_csv(look_up_items['file'], index_col=look_up_items['index_col'])
+        items_full = pd.read_csv(look_up_items['file'],
+                                 index_col=look_up_items['index_col'])
         columns_selection = [
-            'racename', 'date', 'month', 'imlink', 'city', 'image_url', 'logo_url',
-            'region', 'images', 'country_code', 'lat', 'lon', 'is_70.3', 'wc_slots',
-            'entrants_count_avg', 'run_score', 'bike_sinusoity', 'bike_score', 'attractivity_score',
-            'distance_to_nearest_airport', 'distance_to_nearest_airport_international',
+            'racename', 'date', 'month', 'imlink', 'city', 'image_url',
+            'logo_url', 'region', 'images', 'country_code', 'lat', 'lon',
+            'is_70.3', 'wc_slots', 'entrants_count_avg', 'run_score',
+            'bike_sinusoity', 'bike_score', 'attractivity_score',
+            'distance_to_nearest_airport',
+            'distance_to_nearest_airport_international',
             'n_hotels', 'n_restaurants', 'n_entertainment'
         ]
         items = items_full.loc[:, columns_selection]
         # map info
         items_map = items_full.loc[:, [
-            'run_elevation_map', 'bike_elevation_map', 'weather_icon', 'weather_summary',
-            'bike_elevationGain', 'run_elevationGain'
+            'run_elevation_map', 'bike_elevation_map', 'weather_icon',
+            'weather_summary', 'bike_elevationGain', 'run_elevationGain'
         ]]
-        items_map['run_elevation_map'] =  items_map['run_elevation_map'].map(lambda x: json.loads(x))
-        items_map['bike_elevation_map'] =  items_map['bike_elevation_map'].map(lambda x: json.loads(x))
+        items_map['run_elevation_map'] = items_map['run_elevation_map'].map(
+            lambda x: json.loads(x)
+        )
+        items_map['bike_elevation_map'] = items_map['bike_elevation_map'].map(
+            lambda x: json.loads(x)
+        )
         return items
 
 
@@ -111,5 +119,3 @@ def get_items_map(raceId='boulder'):
     map_dict = items_map.loc[raceId].to_dict()
     map_dict['raceId'] = raceId
     return map_dict
-
-
