9a2d63ae3e4f57be90e1c3bc63e86f0b9978ebd4
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-09-06 13:55:16 -0700

    date and location transformer

--- ETL-pipeline/transformers.py
+++ ETL-pipeline/transformers.py
@@ -109,3 +109,44 @@ class FillMissingRegions(BaseEstimator, TransformerMixin):
                 lambda x: missing_regions[x['race']] if x['race'] in missing_regions else x['region']
             ).rename('region')
         ], axis=1)
+
+
+class GetLatestDateAndLocation(BaseEstimator, TransformerMixin):
+    """
+    Ensure date and location are the latest available for each race
+    """
+
+    @staticmethod
+    def getDateAndLocation(race, date, month, city, descriptions):
+        to_check = descriptions.get(race, False)
+        if not to_check:
+            # maybe need lowercase
+            to_check = descriptions.get(race.lower(), False)
+        new_date = to_check['date'] if to_check is not False else date if date else ''
+        if type(new_date) == str and 'TBD' in new_date:
+            new_date = f"TBD {month:02}-{new_date.split('TBD ')[1]}"
+        return pd.Series({
+            'date': new_date,
+            'city': to_check['location'] if to_check is not False else city
+        })
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # make sure to have latest date and location from latest download
+        descriptions = {}
+        with open("./../data/races/races-description.jl") as f:
+            for line in f.readlines():
+                data = json.loads(line.strip())
+                if (data.get('id', "TBD") != "TBD"):
+                    descriptions[data['id']] = data
+                else:
+                    descriptions[f"TBD_{data['name']}"] = data
+
+        return pd.concat([
+            X.transpose()
+                .apply(lambda x: self.getDateAndLocation(x['race'], x['date'], x['month'], x['city'], descriptions))
+                .transpose(),
+            X.loc[:, [col for col in X.columns if col not in ['date', 'city']]]
+        ], axis=1)
