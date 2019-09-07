e38c05198cd516d071779f667cca8cc5a011bb3a
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-09-06 20:50:10 -0700

    extract geo features

--- ETL-pipeline/transformers.py
+++ ETL-pipeline/transformers.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -147,7 +148,23 @@ class SelectColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         columns_to_keep = [
             'race', 'racename', 'date', 'month', 'imlink', 'city', 'image_url',
-            'logo_url', 'region', 'images', 'country_code'
+            'logo_url', 'region', 'images', 'country_code', 'lat', 'lon',
+            'n_years_existance', 'entrants_count_avg', 'run_sinusoity',
+            'run_distance', 'run_elevationGain', 'run_score', 'bike_sinusoity',
+            'bike_distance', 'bike_elevationGain', 'bike_score', 'swim_distance',
+            'swim_type'
+            # 'ironkids_race', 'attractivity_score',
+            # 'perc_entrants_from_country', 'perc_entrants_from_region',
+            # 'perc_female', 'wc_slots', 'distance_to_nearest_shoreline',
+            # 'distance_to_nearest_airport',
+            # 'distance_to_nearest_airport_international', 'n_metropolitan_cities',
+            # 'n_hotels', 'n_restaurants', 'n_entertainment', 'n_nightlife',
+            # 'n_shops', 'n_bike_shops', 'n_pools', 'n_athletic_centers',
+            # 'n_fitness_centers', 'weather_icon', 'weather_summary',
+            # 'temperatureMin', 'temperatureMax', 'apparentTemperatureMin',
+            # 'apparentTemperatureMax', 'swim_min', 'swim_mean', 'swim_max',
+            # 'bike_min', 'bike_mean', 'bike_max', 'run_min', 'run_mean', 'run_max',
+            # 'run_elevation_map', 'bike_elevation_map', 'is_70.3'
         ]
         return X.loc[:, columns_to_keep]
 
@@ -190,5 +207,80 @@ class ComputeRaceHistoryStats(BaseEstimator, TransformerMixin):
             X,
             X.transpose()
              .apply(lambda x: self.get_race_participation(x['race'], races_entrants_count))
+             .transpose()
+        ], axis=1)
+
+
+class ComputeRaceRouteFeatures(BaseEstimator, TransformerMixin):
+    """
+    Extract info from GPS tracks and co
+    """
+
+    # hash for swim type in db
+    swim_type = {
+        'h': 'harbor',
+        'o': 'ocean',
+        'l': 'lake',
+        'r': 'river'
+    }
+
+    @staticmethod
+    def get_angle_between(v1, v2, degrees=False):
+        angle = np.math.atan2(np.linalg.det([v1, v2]), np.dot(v1, v2))
+        if degrees:
+            angle = np.degrees(angle)
+        return angle
+
+    def get_successive_angles(self, x, y):
+        x_diff = x[1:] - x[:-1]
+        y_diff = y[1:] - y[:-1]
+        angles = []
+        for i in range(len(x_diff) - 1):
+            v1 = [x_diff[i], y_diff[i]]
+            v2 = [x_diff[i + 1], y_diff[i + 1]]
+            angles.append(np.abs(self.get_angle_between(v1, v2, degrees=True)))
+        return np.array(angles)
+
+    def extract_geo_info(self, race_map, race_info):
+        # get lat/lon by averaging run points
+        run_map = json.loads(race_map)['run']
+        run_data = np.array(run_map['points'])
+        lon = run_data[:, 0].mean()
+        lat = run_data[:, 1].mean()
+
+        # sinusoity of run route
+        run_sinusoity = self.get_successive_angles(run_data[:, 0], run_data[:, 1]).mean()
+
+        # sinusoity of bike route
+        bike_map = json.loads(race_map)['bike']
+        bike_data = np.array(bike_map['points'])
+        bike_sinusoity = self.get_successive_angles(bike_data[:, 0], bike_data[:, 1]).mean()
+
+        # infos
+        infos = json.loads(race_info)
+
+        return pd.Series({
+            'lat': lat,
+            'lon': lon,
+            'run_sinusoity': run_sinusoity,
+            'run_distance': infos['run']['distance'],
+            'run_elevationGain': infos['run']['elevationGain'],
+            'run_score': infos['run']['score'],
+            'bike_sinusoity': bike_sinusoity,
+            'bike_distance': infos['bike']['distance'],
+            'bike_elevationGain': infos['bike']['elevationGain'],
+            'bike_score': infos['bike']['score'],
+            'swim_distance': infos['swim']['distance'],
+            'swim_type': self.swim_type[infos['swim']['type']]
+        })
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return pd.concat([
+            X,
+            X.transpose()
+             .apply(lambda x: self.extract_geo_info(x['map'], x['info']))
              .transpose()
         ], axis=1)
